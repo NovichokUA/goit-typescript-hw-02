ad20591b97315fc7e03fa05b5823d04795a31ef1
NovichokUA <zubr7333@gmail.com>
2024-04-09 08:46:47 +0300

    add task1

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,10 +1,25 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
+let age: number;
+let name: string;
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
 
-let age = 50;
-let name = 'Max';
-let toggle = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+let callback = (a: number): number => {
+  return 100 + a;
+};
+
+// let callback: (a: number) => number = (a) => {
+//   return 100 + a;
+// };
+
+age = 50;
+name = "Max";
+toggle = true;
+empty = null;
+notInitialize;
+callback = (a) => {
+  return 100 + a;
+};
 
 export {};
